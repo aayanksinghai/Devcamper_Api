f195db9834b7ca247688986db262951d81f84c45
aayanksinghai <aayanksinghai02@gmail.com>
2020-06-26 21:29:47 +0530

    Route API MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
+const connectDB = require('./config/db')
 
 //Load env vars
 dotenv.config({ path: './config/config.env'})
 
+//Connect to Database
+connectDB()
+
 const app = express()
 
+// Body Parser
+app.use(express.json())
+
+// Dev logging middleware
+if(process.env.NODE_ENV === 'development')
+{
+    app.use(morgan('dev'))
+}
+
+// Mount Routers
+app.use('/api/v1/bootcamps', require('./routes/bootcamp'))
+
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)) 
+
+
+// Handle Unhandle promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`)
+    //Close server & exit process
+    server.close(() => process.exit(1))
+ })
