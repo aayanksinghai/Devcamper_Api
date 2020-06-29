ee489d6147b1c7cf0355111ac3e79cb8946baec9
aayanksinghai <aayanksinghai02@gmail.com>
2020-06-30 01:53:16 +0530

    Advanced Middleware

--- routes/bootcamp.js
+++ routes/bootcamp.js
@@ -6,6 +6,9 @@ const { getBootcamp,
         deleteBootcamp,
         getBootcampsInRadius, bootcampPhotoUpload } = require('../controllers/bootcamps')
 
+const advancedResults = require('../middleware/advancedResults')
+const Bootcamp = require('../models/Bootcamp')
+
 // Include other resources routers
 
 const courseRouter = require('./courses')
@@ -24,7 +27,7 @@ router.route('/:id/photo').put(bootcampPhotoUpload)
  
 router
     .route('/')
-    .get(getBootcamps)
+    .get(advancedResults(Bootcamp, 'courses'), getBootcamps)
     .post(createBootcamp)
 
 router
